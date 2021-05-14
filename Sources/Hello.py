9d5693f0ae8fd074b99cb766bb42fa6b7f24e8c1
Heanmogo <heanmogo97@hotmail.com>
2021-05-14 01:09:04 -0500

    cambios en csv

--- Sources/Hello.py
+++ Sources/Hello.py
@@ -2,6 +2,8 @@ print("Jelou")
 print("Programa para DDBD v1.0")
 
 import numpy as np
+import pandas
+
 #Materiales
 fc=210                 #kgf/cm2        f'c del concreto
 fy=4200                #kgf/cm2        Fy del acero
@@ -10,39 +12,68 @@ Ec=15000*np.sqrt(fc)   #kgf/cm2        Módulo de elasticidad del concreto
 Es=2*10^6              #kgf/cm2        Módulo de elasticidad del acero
 λc =2.4                #tnf/m3         Peso especifico del concreto
 
-
-
 #Secciones
+print("SECCIONES:")
 ##Columnas
-ac=0.5                  #m         Ancho de columna (Long. en X)
-bc=0.5                  #m         Largo de columna (Long. en Y)
-C1=[ac,bc]
+dataC = "Sources\Secciones\Columnas.csv"
+C=pandas.read_csv(dataC,header=0)
 
+print(C)
 
 ##Vigas (Beam)
-bb=0.25             #m         Ancho de viga
-hb=0.4              #m         Peralte de viga
-V1=[bb,hb]
+dataB = "Sources\Secciones\Vigas.csv"
+B=pandas.read_csv(dataB,header=0)
+
+print(B)
 
 ##Muros (Wall)
-tw=0.25             #m         Espesor de muro
-Lw=5                #m         Largo de muro
-W1=[tw,Lw]
+dataW = "Sources\Secciones\Muros.csv"
+W=pandas.read_csv(dataW,header=0)
+
+print(W)
 
 ##Losas
-hl=0.20             #m         Espesor de losa aligerada
-ql=300              #kgf/m2    Peso de losa aligerada /m2
-L1=[hl,ql]
+dataL = "Sources\Secciones\Losas.csv"
+L=pandas.read_csv(dataL,header=0)
 
-#Nodos
+print(L)
 
 
 #Elementos
+print("ELEMENTOS:")
 ##Columnas
-##Vigas
-##Muros
-##Losas
+dataCe = "Sources\Elementos\Columnas.csv"
+Ce=pandas.read_csv(dataCe,header=0)
 
+print(Ce)
+
+##Vigas
+dataBeX = "Sources\Elementos\Vigas X.csv"
+BeX=pandas.read_csv(dataBeX,header=0)
+
+print(BeX)
+
+dataBeY = "Sources\Elementos\Vigas Y.csv"
+BeY=pandas.read_csv(dataBeY,header=0)
+
+print(BeY)
+
+##Muros
+dataWeX = "Sources\Elementos\Muros X.csv"
+WeX=pandas.read_csv(dataWeX,header=0)
+
+print(WeX)
+
+dataWeY = "Sources\Elementos\Muros Y.csv"
+WeY=pandas.read_csv(dataWeY,header=0,)
+
+print(WeY)
+
+##Losas
+dataLe = "Sources\Elementos\Losas.csv"
+Le=pandas.read_csv(dataLe,header=0)
+
+print(Le)
 
 
 
